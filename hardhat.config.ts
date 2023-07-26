1d9702a848d0061d8d7516799ed7e6a7e133039c
karthik-buk <karthikn503@gmail.com>
2023-07-26 17:29:54 +0530

    Add foundary

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,32 +1,33 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-foundry";
 
 const config: HardhatUserConfig = {
-	solidity: {
-		version: "0.8.19",
-		settings: {
-			optimizer: {
-				enabled: true,
-				runs: 10000,
-			},
-		},
-	},
-	networks: {
-		mumbai: {
-			url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_MUMBAI_API_KEY}`,
-			accounts:
-				process.env.PRIVATE_KEY_MUMBAI !== undefined
-					? [process.env.PRIVATE_KEY_MUMBAI]
-					: [],
-		},
-	},
-	gasReporter: {
-		enabled: process.env.REPORT_GAS !== undefined,
-		currency: "USD",
-	},
-	etherscan: {
-		apiKey: process.env.POLYGONSCAN_API_KEY,
-	},
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000,
+      },
+    },
+  },
+  networks: {
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_MUMBAI_API_KEY}`,
+      accounts:
+        process.env.PRIVATE_KEY_MUMBAI !== undefined
+          ? [process.env.PRIVATE_KEY_MUMBAI]
+          : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
+  },
 };
 
 export default config;
