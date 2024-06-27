28f65d4d290449cd17eab777fbf6e9110d295d3b
karthik-buk <karthikn503@gmail.com>
2024-06-27 17:25:12 +0530

    Updated the NFT name

--- constants/index.ts
+++ constants/index.ts
@@ -6,8 +6,8 @@ export const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY as string;
 export const USDC_CONTRACT = process.env.USDC_CONTRACT as string;
 export const BUK_WALLET = process.env.BUK_WALLET as string;
 export const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY as string;
-export const BUK_NFT_NAME = "BukProtocol Bookings";
-export const BUK_POS_NFT_NAME = "BukProtocol Bookings - Proof of Stay";
+export const BUK_NFT_NAME = "Buk Hotel bookings";
+export const BUK_POS_NFT_NAME = "Buk Hotel bookings - Proof of Stay";
 export const ROYALTIES = {
   BUK_ROYALTY_PERCENTAGE: Number(process.env.BUK_ROYALTY_PERCENTAGE as string),
   HOTEL_ROYALTY_PERCENTAGE: Number(
